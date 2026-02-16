e31c5918f08605fcb7f1d20918c212c16456f8a1
Sandro Gakharia <sandrogakharia@gmail.com>
2026-02-16 15:13:36 +0400

    Add logic to detect suspicious attempts

--- parser.py
+++ parser.py
@@ -1,7 +1,7 @@
 import re
 
 ip_pattern = r"(\d+\.\d+\.\d+\.\d+)"
-status_code = r"\s(\d{3})\s"
+status_code = r"(HTTP/1.1\")\s(\d{3})\s"
 dictionary = {}
 
 with open("server.log") as file:
@@ -11,11 +11,19 @@ with open("server.log") as file:
         if (match_ip and match_status):
             if match_ip.group() in dictionary:
                 dictionary[match_ip.group()]["count"] += 1
-                dictionary[match_ip.group()]["status_codes"].append(match_status.group(1))
+                dictionary[match_ip.group()]["status_codes"].append(match_status.group(2))
             else:
                 dictionary[match_ip.group()] = {
                     "count": 1,
-                    "status_codes": [match_status.group(1)]
+                    "status_codes": [match_status.group(2)]
                 }
 
-print(dictionary)
+for ip, data in dictionary.items():
+    failed_logins = data["status_codes"].count("401")
+    forbidden =  data["status_codes"].count("403")
+    not_found = data["status_codes"].count("404")
+    report_line = f"IP = {ip:15} | Requests: {data['count']}"
+    if (failed_logins + forbidden + not_found > 3):
+        print(report_line + f" 🚨 SECURITY ALERT: {failed_logins + forbidden + not_found} suspicious attempts detected! 🚨")
+    else:
+        print(report_line)
