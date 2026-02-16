1811bb5c5fbd9de8b6afdac37a0d67dd53d1dbd8
Sandro Gakharia <sandrogakharia@gmail.com>
2026-02-16 18:23:46 +0400

    Add custom log file inclusion from command line

--- parser.py
+++ parser.py
@@ -1,10 +1,18 @@
 import re
+import sys
 
 ip_pattern = r"(\d+\.\d+\.\d+\.\d+)"
 status_code = r"(HTTP/1.1\")\s(\d{3})\s"
 dictionary = {}
+threshold = 3
 
-with open("server.log") as file:
+if len(sys.argv) < 2:
+    print("You forgot the file name.")
+    sys.exit()
+elif len(sys.argv) == 3:
+    threshold = int(sys.argv[2])
+
+with open(sys.argv[1]) as file:
     for line in file:
         match_ip = re.search(ip_pattern, line)
         match_status = re.search(status_code, line)
@@ -23,7 +31,7 @@ for ip, data in dictionary.items():
     forbidden =  data["status_codes"].count("403")
     not_found = data["status_codes"].count("404")
     report_line = f"IP = {ip:15} | Requests: {data['count']}"
-    if (failed_logins + forbidden + not_found > 3):
+    if (failed_logins + forbidden + not_found > threshold):
         print(report_line + f" 🚨 SECURITY ALERT: {failed_logins + forbidden + not_found} suspicious attempts detected! 🚨")
     else:
         print(report_line)
